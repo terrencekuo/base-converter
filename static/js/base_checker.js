6ece421bf2bc3016847fb88e1a7ec80407063e5b
terrencekuo <kuuott@gmail.com>
2015-12-23 04:51:43 -0500

    done

--- static/js/base_checker.js
+++ static/js/base_checker.js
@@ -1,60 +1,111 @@
-var states = ["start", "decimal", "hexidecimal"];
-var cState = "start";
+function addBinary(){
+	var input = document.getElementById("input").value;
+	var len = input.length;
+
+	if (len == 0)
+		document.getElementById("input").value = "b";
+	else
+	{
+		if(input.substring(0,1) == "b" && len == 1)
+			return
+		document.getElementById("input").value = "b";
+	}
+}
+
+function addHex(){
+	var input = document.getElementById("input").value;
+	var len = input.length;
+
+	if (len == 0)
+		document.getElementById("input").value = "0x";
+	else
+	{
+		if(input.substring(0,1) == "0" && input.substring(0,1) == "x" && len == 2)
+			return
+		document.getElementById("input").value = "0x";
+	}
+}	
+
+function addDecimal(){
+	document.getElementById("input").value = "";
+}	
+
+function validate(event){
+	var keyCode = ('which' in event) ? event.which : event.keyCode; 		// obtain ascii character from keyboard
+	var ctrlDown = event.ctrlKey||event.metaKey 								// Mac support
+
+	var numerical = (keyCode >= 48) && (keyCode <=57); 						// allow only numerical values of 0 to 9 to be entered 
+	var lowAlpha = ((keyCode >= 97) && (keyCode <=102)) || keyCode == 120;	// allow a-f, x
+	var upperAlpha = ((keyCode >= 65) && (keyCode <=70))|| keyCode == 88 ;	// allow A-F, x
+	var modifier = (keyCode == 13) || (keyCode == 8) || 
+					(keyCode == 37) || (keyCode == 39); 					// allow only backspace, enter, left, right
+	var copy = ctrlDown && keyCode==67;											// c
+	var cut = ctrlDown && keyCode==86;											// x
+	var paste = ctrlDown && keyCode==88;										// v
+	var refresh = ctrlDown && keyCode==114;										// r
+
+	return numerical || modifier || lowAlpha || upperAlpha || modifier || copy || cut || paste || refresh;
+}
 
 function baseChecker(){
 	var input = document.getElementById("input").value;
 	var len = input.length;
-	input = input.substring(len-1,len); // obtain each char
-	console.log(input);
-	switch(cState){
-		case("start"):
-			if(!isNaN(input))
-			{	
-				val = parseInt(input);
-				if (val == 0)
-					cState = "zero";
-				else
-					cState = "decimal"
-				document.getElementById("decimal").checked = true;
-			}
-			else
-			{
-				console.log("error in start");
-				document.getElementById("decimal").checked = false;	
-				document.getElementById("hexidecimal").checked = false;	
-				document.getElementById("binary").checked = false;	
-			}
-			break;
-		case("zero"):
-			if (len == 2 && input == "x")
-			{
-				cState = "hexidecimal";
-				document.getElementById("hexidecimal").checked = true;
-			}
-			else{
-				if(isNaN(input))
-				{
-					console.log("error in zero");
-					document.getElementById("decimal").checked = false;	
-					document.getElementById("hexidecimal").checked = false;	
-					document.getElementById("binary").checked = false;	
-				}
-				else
-				{
-					if(parseInt(input) != 0)
-					{
-						cState = "decimal";
-						document.getElementById("decimal").checked = true;
-					}
-				}
-			}
-			break;
-		case("decimal"):
-			break;
-		case("hexidecimal"):
-			break;
-		default:
-			console.log("error in default")
+
+	// check if decimal
+	if((input.match(/^[0-9]+$/) != null) && len > 0)
+	{
+		if(len > 0)
+			document.getElementById("submit").disabled = false;
+		else
+			document.getElementById("submit").disabled = true;
+		document.getElementById("decimal").checked = true;
+		return;
 	}
 
+	var	firstChar;
+	var secondChar;
+
+	if (len >= 1)
+		firstChar = input.substring(0,1);
+
+	// check if binary
+	if (firstChar == 'b' && checkBinary(input.substring(1)))
+	{
+		if(len >= 2)
+			document.getElementById("submit").disabled = false;
+		else
+			document.getElementById("submit").disabled = true;
+		document.getElementById("binary").checked = true;
+		return;
+	}
+
+
+	if (len >= 2)
+		secondChar = input.substring(1,2);
+
+	// check if hexidecimal
+	if (firstChar == '0' && secondChar == 'x')
+	{
+		if(len>=3)
+			document.getElementById("submit").disabled = false;
+		else
+			document.getElementById("submit").disabled = true;
+		document.getElementById("hexidecimal").checked = true;
+		return;
+	}
+
+	document.getElementById("decimal").checked = false;	
+	document.getElementById("hexidecimal").checked = false;	
+	document.getElementById("binary").checked = false;
+	document.getElementById("submit").disabled = true; 
+}
+
+function checkBinary(val){
+	len = val.length;
+	for(var i = 0; i<len; i++)
+	{
+		if(val[i] != "0" && val[i] != "1") 
+			return false;
+	}
+	return true;
 }
--- static/js/base_converter.js
+++ static/js/base_converter.js
@@ -19,6 +19,42 @@ function Converter(){
 	var type = getRadioVal( document.getElementById('base_converter'), 'baseType' );
 	console.log(type);
 	var div = document.createElement('div');
+	var span = document.createElement('span');
+	span.innerHTML = "x"
+	span.setAttribute("id", "close");
+	span.onclick = function(){
+		this.parentElement.parentElement.removeChild(this.parentElement);
+	}
+	div.className = "fragment";
+	div.appendChild(span);
+
+	var div1 = document.createElement('div');
+	div1.setAttribute("id", "floating");
+
+	var p = document.createElement('p');
+	p.innerHTML = "input ";
+	div1.appendChild(p);					
+
+	var p = document.createElement('p');
+	p.innerHTML = type + ": " + input +" ";
+	div1.appendChild(p);
+
+	div.appendChild(div1);				
+
+	if (type == "binary")
+	{
+		var val = parseInt(input.substring(1));
+		var decimalValue = 0;
+		var i = 0;
+		while(val != '')
+		{
+			lastInt = val % 10;
+			decimalValue += lastInt*Math.pow(2,i);
+			val = Math.floor(val / 10);
+			i++;
+		}
+		input = decimalValue.toString();
+	}
 
 	// convert string to int
 	var val = parseInt(input);
@@ -31,16 +67,37 @@ function Converter(){
 	};
 
 	var textNode;
+
+	var div1 = document.createElement('div');
+	div1.setAttribute("id", "floating");
+
+	var p = document.createElement('p');
+	p.innerHTML = "output ";
+	div1.appendChild(p);					
+
 	// iterate through output OBJ
+
 	for(var key in output){
 		if (key != type)
 		{
-			var string = key + ": " + output[key]+ " ";
-			textNode = document.createTextNode(string); // create textNode w/ string
-			div.appendChild(textNode);					// add text to Node
+			if (key == "hexidecimal")
+				output[key] = "0x" + output[key];
+			var p = document.createElement('p');
+			p.innerHTML += key + ": " + output[key]+ " ";
+			div1.appendChild(p);
 		}
 	}
+		
 
-	document.getElementById("h_output").appendChild(div);
+	div.appendChild(div1);	
+
+	var parent = document.getElementById("h_output");
+	parent.insertBefore(div, parent.firstChild)
+
 	document.getElementById("input").value = '';	// clear value in input field
+
+	document.getElementById("decimal").checked = false;	
+	document.getElementById("hexidecimal").checked = false;	
+	document.getElementById("binary").checked = false;
+	document.getElementById("submit").disabled = true; 
 }
